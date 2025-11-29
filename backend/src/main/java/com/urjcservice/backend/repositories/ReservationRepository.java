bd76703fb0605e52d4718372afb89c0f5b5b547f
Guiller-25 <guilleraes@gmail.com>
2025-11-29 21:45:44 +0100

    Trying to fix the routes for sonar
    I think the workflow fail because the routes one is backend and other is Backend.

--- backend/src/main/java/com/urjcservice/backend/repositories/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.urjcservice.backend.repositories;
-
-import com.urjcservice.backend.entities.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-}
