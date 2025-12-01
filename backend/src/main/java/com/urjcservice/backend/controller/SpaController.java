db11bd03eeba378b3ba8c4beb9d7513a339fcbda
Guiller-25 <guilleraes@gmail.com>
2025-12-01 20:01:24 +0100

    Fix Hotspot Sonar backend
    Change the spa controller to fix a hotspot on sonar.

--- backend/src/main/java/com/urjcservice/backend/controller/SpaController.java
+++ backend/src/main/java/com/urjcservice/backend/controller/SpaController.java
@@ -3,12 +3,16 @@ package com.urjcservice.backend.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class SpaController {
 
     //captures all routes except those containing a period (.) or /api
-    @RequestMapping(value = "/**/{path:[^\\.]*}")
+    //@RequestMapping(value = "/**/{path:[^\\.]*}")
+    @GetMapping(value = "/**/{path:[^\\.]*}")
     public String forward(@PathVariable("path") String path) {
         //redirect to index.html so that Angular can handle the routing
         return "forward:/index.html";
