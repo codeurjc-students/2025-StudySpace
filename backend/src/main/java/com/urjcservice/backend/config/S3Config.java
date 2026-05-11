4b065e22da9c6f4e7e0db68337415358fed271d0
Guiller-25 <guilleraes@gmail.com>
2026-05-11 22:07:30 +0200

    Finalized EC2 configuration
    I have finalized the EC2 configuration and now going to create the cloud formation.

--- backend/src/main/java/com/urjcservice/backend/config/S3Config.java
+++ backend/src/main/java/com/urjcservice/backend/config/S3Config.java
@@ -4,25 +4,30 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
-    @Value("${AWS_ACCESS_KEY_ID:test-key}")
+    @Value("${AWS_ACCESS_KEY_ID:}")
     private String accessKey;
 
-    @Value("${AWS_SECRET_ACCESS_KEY:test-secret}")
+    @Value("${AWS_SECRET_ACCESS_KEY:}")
     private String secretKey;
 
     @Bean
     public S3Client s3Client() {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+        var builder = S3Client.builder().region(Region.EU_SOUTH_2);
 
-        return S3Client.builder()
-                .region(Region.EU_SOUTH_2)
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
-                .build();
+        if (accessKey != null && !accessKey.isBlank() && !accessKey.equals("test-key")) {
+            AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+            builder.credentialsProvider(StaticCredentialsProvider.create(credentials));
+        } else {
+            builder.credentialsProvider(DefaultCredentialsProvider.create());
+        }
+
+        return builder.build();
     }
 }
